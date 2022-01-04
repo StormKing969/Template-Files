ab8f80b6b110812a107b8dea9b7b7d798c0427f9
StormKing969 <wijesinghesaj@gmail.com>
2022-01-04 18:00:18 -0500

    update

--- app.js
+++ app.js
@@ -14,14 +14,60 @@ app.listen(3000, function() {
 });
 
 // EJS
-app.set('view engine', 'ejs');
+// jshint esversion:6
 
-// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
-  });
-  
-  // about page
-  app.get('/about', function(req, res) {
-    res.render('pages/about');
-  });
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+// res.send("") => sends one text
+
+
+// res.write("") => used to send multiple texts 
+// res.write("")
+// res.send()
+
+// res.sendFile => sends an entire html file
+
+app.get("/", function(req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;     
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;            
+        default:
+            console.log("Error: Current Day value is: " + currentDay); 
+            break;
+    }
+
+    // Passes the result to list.ejs
+    res.render("list", {kindOfDay: day});
+});
+
+app.listen(3000, function() {
+    console.log("Server is connected to port 3000")
+});
